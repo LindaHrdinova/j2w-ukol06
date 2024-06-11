cf6486f0497773e9b490c06651a00040b77faa34
Linda Hrdinová <hrlin@seznam.cz>
2024-06-11 19:17:17 +0200

    Zpracování úkolu, bod 1-5)

---
+++ src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
@@ -0,0 +1,24 @@
+package cz.czechitas.java2webapps.ukol6.controller;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class VizitkaController {
+
+    @InitBinder
+    public void nullStringBinding(WebDataBinder binder) {
+        //prázdné textové řetězce nahradit null hodnotou
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+
+    @GetMapping("/")
+    public ModelAndView seznam() {
+        return new ModelAndView("seznam");
+    }
+
+}
